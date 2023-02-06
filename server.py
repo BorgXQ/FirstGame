6a8911d471d94b63665e05ec715252d11ff83b4e
BorgXQ <jonathan.widarsa@gmail.com>
2023-02-06 22:51:40 +0800

    Add socket

--- server.py
+++ server.py
@@ -5,3 +5,11 @@ import sys
 server = ""
 port = 5555
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
